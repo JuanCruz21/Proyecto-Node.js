4b908335f06caac2a236a9d7d601f697042a13ec
JuanCruz21 <juancruz.02.2001@gmail.com>
2023-08-23 22:22:14 -0500

    Prueba

--- 05-Path/ls.js
+++ 05-Path/ls.js
@@ -1,14 +1,13 @@
 const fs = require('node:fs/promises')
 // const folder = process.argv['cd ..'] ?? '.'
 fs.readdir('.')
-    .then(files =>{files.forEach(file => {
-            console.log(file)
-        }
-        )
-    }
-    ).catch(err => {
-        if (err) {
-            console.error('error en el directorio: ', err)
-            return;
-        }
+  .then((files) => {
+    files.forEach((file) => {
+      console.log(file)
     })
+  })
+  .catch((err) => {
+    if (err) {
+      console.error('error en el directorio: ', err)
+    }
+  })
--- 05-Path/lsadvance.js
+++ 05-Path/lsadvance.js
@@ -1,36 +1,39 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const pc = require('picocolors')
 
 const folder = process.argv[2] ?? '.'
 
 async function ls (folder) {
-    let files
-    try{
-        files = await fs.readdir(folder)
-    } catch{
-        console.error(`No se puede leer el directorio ${folder} `)
-        process.exit(1)
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(pc.red(`❌ No se puede leer el directorio ${folder} `))
+    process.exit(1)
+  }
+
+  const filespromise = files.map(async (file) => {
+    const filePath = path.join(folder, file)
+    let Stats
+    try {
+      Stats = await fs.stat(filePath) // status - información del archivo
+    } catch {
+      console.error(pc.red(`❌ No se puede leer el directorio ${folder} `))
+      process.exit(1)
     }
+    const isDirectory = Stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = Stats.size.toString()
+    const fileModified = Stats.mtime.toLocaleString()
 
-    const filespromise = files.map(async file => {
-        const filePath = path.join(folder, file)
-        let Stats
-        try{
-            Stats = await fs.stat(filePath) //status - información del archivo
-        }catch{
-            console.error(`No se puede leer el directorio ${folder} `)
-            process.exit(1)
-        }
-        const isDirectory = Stats.isDirectory()
-        const fileType = isDirectory ? 'd' : 'f'
-        const fileSize = Stats.size
-        const fileModified = Stats.mtime.toLocaleString()
+    return `${pc.cyan(fileType)} ${pc.green(file.padEnd(20))} ${pc.yellow(
+      fileSize.padStart(10)
+    )} ${pc.blue(fileModified)}`
+  })
+  const filesInfo = await Promise.all(filespromise)
 
-        return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
-    })
-    const filesInfo = await Promise.all(filespromise)
-
-    filesInfo.forEach(fileInfo => console.log(fileInfo))
+  filesInfo.forEach((fileInfo) => console.log(fileInfo))
 }
 
 ls(folder)
