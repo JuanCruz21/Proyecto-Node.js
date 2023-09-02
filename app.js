89323cb33db384e53d81377226a6b9010baf931d
JuanCruz21 <juancruz.02.2001@gmail.com>
2023-09-02 00:43:25 -0500

    Nuevos cambios

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const movies = require('./movies/movies.json')
+const crypto = require('node:crypto')
 const app = express()
 
 app.disable('x-powered-by')
@@ -7,6 +8,13 @@ app.disable('x-powered-by')
 app.use(express.json())
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filterMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filterMovies)
+  }
   res.json(movies)
 })
 
@@ -16,6 +24,31 @@ app.get('/movies/:id', (req, res) => {
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
   res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate,
+    poster
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
 })
 
 const Puerto = process.env.PORT ?? 1234
