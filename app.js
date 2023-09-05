8a4e686deb858de2a1717ac3afdda94e6fb82e0d
JuanCruz21 <82663723+JuanCruz21@users.noreply.github.com>
2023-09-05 14:04:46 -0500

    Nuevos cambios

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.disable('x-powered-by')
 app.use(express.json())
 
 app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
   const { genre } = req.query
   if (genre) {
     const filterMovies = movies.filter(
